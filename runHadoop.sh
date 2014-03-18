7a89769f66738b61254bcc44f5469e8c39dd02f4
Hongbiao Yang <hyang22@hydra4.eecs.utk.edu>
2014-03-18 16:09:49 -0400

    modified:   runHadoop.sh

--- runHadoop.sh
+++ runHadoop.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-hadoop dfs -mkdir /local_scratch/wordcount
-hadoop dfs -put input /local_scratch/wordcount
+../bin/hadoop dfs -mkdir /local_scratch/wordcount
+../bin/hadoop dfs -put input /local_scratch/wordcount
 
-hadoop dfs -rmr /local_scratch/wordcount/output
-hadoop dfs -rmr /local_scratch/wordcount/tmp
-hadoop dfs -rmr /local_scratch/wordcount/stopword
+../bin/hadoop dfs -rmr /local_scratch/wordcount/output
+../bin/hadoop dfs -rmr /local_scratch/wordcount/tmp
+../bin/hadoop dfs -rmr /local_scratch/wordcount/stopword
 rm -rf classes/*.class
 javac -classpath ../hadoop-core-1.2.1.jar:../lib/commons-cli-1.2.jar -d classes GenIndex.java
 jar -cvf GenIndex.jar -C classes .
 #../bin/hadoop jar wordcount.jar org.myorg.WordCount /user/hyang22/wordcount/input /user/hyang22/wordcount/output
-hadoop jar GenIndex.jar GenIndex /local_scratch/wordcount/input /local_scratch/wordcount/output
+../bin/hadoop jar GenIndex.jar GenIndex /local_scratch/wordcount/input /local_scratch/wordcount/output
 #hadoop dfs -cp /user/hyang22/wordcount/output/part* output/
 #rm -rf index
-#hadoop dfs -get /user/hyang22/wordcount/output/part-r-00000 index
+../bin/hadoop dfs -get /local_scratch/wordcount/output/part-r-00000 index
